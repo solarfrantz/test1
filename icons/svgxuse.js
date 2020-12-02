4a705b04d064e38f2d383ef7fc1ae35c92eb41a0
solarfrantz <francisc.ungureanu@solarwinds.com>
2020-12-02 13:23:35 +0000

    Trigger deployment

--- icons/svgxuse.js
+++ icons/svgxuse.js
@@ -79,7 +79,7 @@
         checkUseElems = function () {
             var base;
             var bcr;
-            var fallback = "https://i.icomoon.io/public/temp/659d899e55/UntitledProject/symbol-defs.svg?64d19c0f"; // optional fallback URL in case no base path to SVG file was given and no symbol definition was found.
+            var fallback = "https://i.icomoon.io/public/temp/659d899e55/UntitledProject/symbol-defs.svg?9b825d07"; // optional fallback URL in case no base path to SVG file was given and no symbol definition was found.
             var hash;
             var href;
             var i;
